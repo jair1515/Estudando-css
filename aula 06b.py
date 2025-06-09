782d717cf114ec97607882224813f495da1f3f72
jair1515 <135368945+jair1515@users.noreply.github.com>
2025-06-09 05:52:18 +0000

    Aula 06 e desafios curso em video PYTHON

---
+++ aula 06b.py
@@ -0,0 +1,4 @@
+n = input('Digite Algo')    
+#print(n.isnumeric())
+#print(n.isalpha())
+#print(n.isalnum())
--- aula06.py
+++ aula06.py
@@ -1,4 +1,5 @@
-number1 = int(input('digite um número:')) 
-number2 = int(input('digite um número:')) 
-s = number1 + number2
-print('A soma entre' , number1, 'e' , number2 , 'é' , s) 
+n1 = int(input('digite um número:')) 
+n2 = int(input('digite um número:')) 
+s = n1 + n2
+# print('A soma entre' , number1, 'e' , number2 , 'é' , s) 
+print('A soma entre {} e {} vale {}'.format(n1, n2, s))
